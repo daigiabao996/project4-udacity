479008aa440212ff92a7931f72a721723b46b11c
daigiabao996 <vanquocquanbao96@gmail.com>
2022-11-26 18:52:02 +0700

    first commit

---
+++ upload_docker.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=baovqq/project04
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: $dockerpath"
+docker login -u baovqq
+docker tag api $dockerpath:latest
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath:latest
+
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
